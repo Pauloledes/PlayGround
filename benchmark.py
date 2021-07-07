1d3d250c846ff48f298f88a0e301bd2893762ff8
pledes <paul.ledesma@hotmail.fr>
2021-07-07 11:19:28 +0200

    Relief of the memory

--- benchmark.py
+++ benchmark.py
@@ -2,9 +2,11 @@ import logging
 import treefiles as tf
 from eulerian_magnification import eulerian_magnification
 from eulerian_magnification.io import load_video_float, save_video
+import gc
+import numpy as np
 
 
-def apply_evm(vid, fps, videoname, dico=None):
+def apply_evm(vid, fps, videoname:str, dico=None):
     if dico is None:
         dico = {'amplification_factor': 100, 'lower_hertz': 0, 'upper_hertz': 1, 'pyramid_levels': 4}
 
@@ -20,38 +22,43 @@ def apply_evm(vid, fps, videoname, dico=None):
         amplification=amplification_factor,
         pyramid_levels=pyramid_levels,
     )
-    save_video(vid, fps, 'data/' + str(videoname))
+    save_video(vid, fps, videoname)
     return vid
 
+
+def run_loop(great_dico, dico, vid, fps, videos):
+    for j in range(len(great_dico['upper_hertz'])):
+        dico['upper_hertz'] = great_dico['upper_hertz'][j]
+
+        try:
+            for k in range(len(great_dico['amplification_factor'])):
+                dico['amplification_factor'] = great_dico['amplification_factor'][k]
+        except TypeError:
+            dico['amplification_factor'] = great_dico['amplification_factor']
+
+        try:
+            for l in range(len(great_dico['pyramid_levels'])):
+                dico['pyramid_levels'] = great_dico['pyramid_levels'][l]
+        except TypeError:
+            dico['pyramid_levels'] = great_dico['pyramid_levels']
+
+        name = f"EVM_freqmin={str(dico['lower_hertz'])}_freqmax={str(dico['upper_hertz'])}" \
+               f"_ampli={str(dico['amplification_factor'])}.avi"
+        print(name)
+        video_name = f'data/{name}'
+        apply_evm(vid, fps, video_name, dico=dico)
+        videos.append(video_name)
+        gc.collect()
 
 def apply_multiple_evms(vid, fps, great_dico):
     print(f'Applying EVM for set of parameters : {great_dico}')
     videos = []
     for i in range(len(great_dico['lower_hertz'])):
-
         dico = {}
         dico['lower_hertz'] = great_dico['lower_hertz'][i]
-
-        for j in range(len(great_dico['upper_hertz'])):
-            dico['upper_hertz'] = great_dico['upper_hertz'][j]
-
-            try:
-                for k in range(len(great_dico['amplification_factor'])):
-                    dico['amplification_factor'] = great_dico['amplification_factor'][k]
-            except TypeError:
-                dico['amplification_factor'] = great_dico['amplification_factor']
-
-            try:
-                for l in range(len(great_dico['pyramid_levels'])):
-                    dico['pyramid_levels'] = great_dico['pyramid_levels'][l]
-            except TypeError:
-                dico['pyramid_levels'] = great_dico['pyramid_levels']
-
-            name = f"EVM_freqmin={str(dico['lower_hertz'])}_freqmax={str(dico['upper_hertz'])}" \
-                   f"_ampli={str(dico['amplification_factor'])}.avi"
-            print(name)
-            new_vid = apply_evm(vid, fps, name, dico=dico)
-            videos.append(new_vid)
+        run_loop(great_dico, dico, vid, fps, videos)
+        del dico
+        gc.collect()
 
     return videos
 
--- main_function.py
+++ main_function.py
@@ -1,4 +1,6 @@
+import gc
 import logging
+import shutil
 from typing import NamedTuple, List
 import matplotlib.pyplot as plt
 import numpy as np
@@ -6,63 +8,57 @@ import treefiles as tf
 from eulerian_magnification.io import load_video_float
 from matplotlib.animation import FuncAnimation, PillowWriter
 from benchmark import apply_multiple_evms
+import os
+import numpy as np
+
 
 class Param(NamedTuple):
     name: str
     values: List[float]
 
 
-def main(x: Param, y: Param, videos, final_filename):
+def main(x: Param, y: Param, videos_dir, final_filename):
     fig, axs = prepare_canvas(x, y, figsize=(6, 6))
     nx, ny = len(x.values), len(y.values)
     set_labels(x, y, axs)
-
-    # Passage du RGB au BGR
-    for n in range(len(videos)):
-        videos[n] = videos[n][..., ::-1]
 
     nb_frames, dim_y, dim_x, dim_z = vid.shape
 
     imgs = []
     try:
         for ax in axs.ravel():
-            imgs.append(ax.imshow(videos[0][0, ...]))
-    except:
-        imgs.append(axs.imshow(videos[0][0, ...]))
+            imgs.append(ax.imshow(np.load(f'{videos_dir[0]}/F_0.npy')))
+    except Exception as e:
+        imgs.append(axs.imshow(np.load(f'{videos_dir[0]}/F_0.npy')))
 
     def update(frame):
         for n in range(nx * ny):
-            imgs[n].set_array(videos[n][frame, ...])
+            imgs[n].set_array(np.load(f'{videos_dir[n]}/F_{frame}.npy'))
         return imgs
 
     print(f'Generating FuncAnimation...')
-
-    ani = FuncAnimation(fig, update, frames=nb_frames, blit=True, interval=300)
+    ani = FuncAnimation(fig, update, frames=nb_frames, blit=True, interval=300, repeat=False)
     plt.tight_layout()
     writergif = PillowWriter(fps=nb_frames)
-    ani.save(filename=str(final_filename) + ".gif", writer=writergif)#, fps=nb_frames)
-
+    ani.save(filename=str(final_filename) + ".gif", writer=writergif)  # , fps=nb_frames)
     plt.show()
-
-
 
 
 def set_labels(x: Param, y: Param, axs):
     for k in ["x", "y"]:
         for i, v in enumerate(locals()[k].values):
-            try :
+            try:
                 ax = (axs if k == "x" else axs.T)[0, i]
-            except :
-                try :
+            except:
+                try:
                     ax = (axs if k == "x" else axs.T)[i]
-                except :
-                    ax=axs
+                except:
+                    ax = axs
             try:
                 getattr(ax, f"set_{k}ticks")([sum(getattr(ax, f"get_{k}lim")()) / 2])
                 getattr(ax, f"set_{k}ticklabels")([f"{v}"])
             except TypeError:
                 pass
-
 
 
 def prepare_canvas(x: Param, y: Param, title=None, **kw):
@@ -74,7 +70,7 @@ def prepare_canvas(x: Param, y: Param, title=None, **kw):
     nx, ny = len(x.values), len(y.values)
     fig, axs = plt.subplots(nrows=ny, ncols=nx, sharex="col", sharey="row", **kw)
 
-    try :
+    try:
         for ii in axs.ravel():
             ii.tick_params(**pltnot)
     except:
@@ -92,6 +88,31 @@ def prepare_canvas(x: Param, y: Param, title=None, **kw):
     return fig, axs
 
 
+def save_frames(videos):
+    videos_dir = []
+
+    for v in videos:
+        video_to_work_with, _ = load_video_float(v)
+        string_v = v[:-4]
+        videos_dir.append(string_v)
+
+        if not os.path.exists(string_v):
+            os.mkdir(string_v)
+
+        for i, frame in enumerate(video_to_work_with):
+            frame = frame[..., ::-1]
+            np.save(f'{string_v}/F_{i}', frame)
+
+        del video_to_work_with
+        gc.collect()
+    return videos_dir
+
+
+def delete_dirs(videos_dir):
+    for folder in videos_dir:
+        shutil.rmtree(folder)
+
+
 def get_random(dims, frames):
     vid = [*dims, 3, frames]
     return np.random.rand(*vid)
@@ -103,14 +124,16 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
     log = tf.get_logger()
 
-    _x = Param("lower_hertz", [0])#, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9])#, 0.1, 0.2, 0.3])  # , 0.4, 0.5])
-    _y = Param("upper_hertz", [1, 2, 3, 4, 5, 6])#, 1.2, 1.4, 1.6])  # , 0.9, 1])
+    _x = Param("lower_hertz", [0, 0.3, 0.4])#, 0.5, 0.6, 0.7, 0.8, 0.9])#, 0.1, 0.2, 0.3])  # , 0.4, 0.5])
+    _y = Param("upper_hertz", [1, 2])  # , 4, 5, 6])#, 1.2, 1.4, 1.6])  # , 0.9, 1])
 
     root = tf.Tree.new(__file__, "data")
     root.file(m="wrist.mp4")
     vid, fps = load_video_float(root.m)
-    great_dico = {'amplification_factor': 100, 'lower_hertz': _x.values, 'upper_hertz': _y.values,
+    great_dico = {'amplification_factor': 10, 'lower_hertz': _x.values, 'upper_hertz': _y.values,
                   'pyramid_levels': 4}
     videos = apply_multiple_evms(vid, fps, great_dico)
+    videos_dir = save_frames(videos)
 
-    main(_x, _y, videos, "testtestvideo")
+    main(_x, _y, videos_dir, "testtestvideo5")
+    delete_dirs(videos_dir)
